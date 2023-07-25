02134530c6630cdb658fa0263a17f7deed87a548
sharanabasappadhammur <sharanabasappa@itplusi.com>
2023-07-25 17:04:02 +0530

    25july
    25july

--- index.js
+++ index.js
@@ -59,6 +59,59 @@ const api = axios.create({
     jar: cookieJar,
 });
 
+// get
+// http://tincaphe.com//api/account/logout
+
+// post
+// http://tincaphe.com//api/account/authenticate
+
+let payload = {
+    grant_type: "password",
+    client_id: "APP",
+    usernameOrEmailAddress: "integrated",
+    password: "welcome",
+    tenancyName: "Default",
+    rememberMe: false
+}
+
+let generatedTokenForAuthenticate;
+function Login() {
+    api.defaults.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36';
+    api.post("http://tincaphe.com//api/account/authenticate",payload)
+        .then(response => {
+            console.log("response...........",response?.data?.result?.access_token?.slice(0,50))
+            generatedTokenForAuthenticate = response?.data?.result?.access_token
+            // setTimeout(()=>{
+            //     Logout()
+            // },10000)
+        })
+        .catch(error => {
+            console.error('Error:', error.message);
+        });
+}
+
+
+function Logout() {
+    let currentTokenForLogout = generatedTokenForAuthenticate
+    console.log("currentTokenForLogout..........",currentTokenForLogout?.slice(0,50))
+    api.defaults.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36';
+    api.defaults.headers.common['Authorization'] = `Bearer ${currentTokenForLogout}`;
+    api.get("http://tincaphe.com//api/account/logout")
+        .then(response => {
+            console.log("Success Logout")
+            Login()
+        })
+        .catch(error => {
+            console.error('Error:', error.message);
+        });
+}
+
+// Login()
+
+setInterval(()=>{
+    Logout()
+},3600000)
+
 
 let generatedToken;
 
@@ -71,11 +124,11 @@ let arabicaGlobalArray = []
 let xeGlobalArray = []
 
 function getTincapheData() {
-    let token2 = generatedToken
+    // let token2 = generatedToken
+    let token = generatedTokenForAuthenticate
     api.defaults.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36';
     const authToken = 'ypblh8z-ejgFJ45pGqrlPyPWMcSeToZ5J6Deolg7EqXZdNtJMrXF55vLBHveiDmakNNP3TLoVY2-NDl64Efwet01zRqemYu4924EUPWTqtW1AbcwOCM10wKFI0kMjlEAY6lgvxYA4kSyi_ij6OieDwyA0mle9sadbWAgbbEi9YxdVHuGM0PKwtd5YCht6kMkY_Ndtlow7F0NL3-hMFWDHnP8j70F2spRbcR3o7tWtpkKsAszgy4DdDOVjApi-qses1HyuKvu_33xF6AhmXGegrUH0wP1297K3WmciT_dP_UY9AtJru_NW6Ox4aOo11zogKMY4yJadZVGWNmxboKgp4TujFTwoJBdPr11dIHxaICngJKzqNGDWh3uHVIjwoIlAxrgBjkgVALIhpq11QI1bcHFJRZvSRr1a__UOQ_6mCXI9DrHJeXNfD-4yD6S41XpiBmh0MYxFHEmjauB7UNkhCCqD2S4M2hFXPlP4l9raP9MDHh3PPnhAG9EDdx5dgvTmU5rOPb3liEPRMwxmu9GwxJwC2pIMf6-qrrlnCEjz0tOrbbe8ZqIL6RxLKjuhYNDFviRVCFqz0QekTo8EAmPGP8eRdQqDbM-2keRa-KAcHh9haCDFGWpqhf3_CjnQYp5ov81tWsMFG2VqK3AghWyaw';
-    api.defaults.headers.common['Authorization'] = `Bearer ${token2}`;
-    console.log("token", token2)
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
     api.post(endpoint)
         .then(response => {
 
@@ -92,7 +145,7 @@ function getTincapheData() {
                     // if (index >= 41 && index <= 42) {
                     const object = {
                         isHighlight: 0,
-                        id: 0,
+                        id: ittirationForRobusta,
                         createdBy: 1,
                         createdOn: "2023-07-08T06:41:51.810Z",
                         updatedBy: 1,
@@ -128,14 +181,14 @@ function getTincapheData() {
                 if (index >= 4 && index <= 7) {
                     const object = {
                         isHighlight: 0,
-                        id: 0,
+                        id: ittirationForArabica,
                         createdBy: 1,
                         createdOn: "2023-07-08T06:41:51.810Z",
                         updatedBy: 1,
                         updatedDtms: "2023-07-08T06:41:51.810Z",
                         idMarket: 2,
                         contractName: arabicaNameList[ittirationForArabica],
-                        lastChng: parseInt(ele.vs[1]),
+                        lastChng: parseFloat(ele.vs[1]),
                         chng: ele.vs[2],
                         percentageVal: ele.vs[3],
                         volume: parseInt(ele.vs[4]),
@@ -166,13 +219,15 @@ function getTincapheData() {
         })
         .catch(error => {
             console.error('Error:', error.message);
-            generateToken()
+            // generateToken()
+            Login()
         });
 }
+
 let localAuthToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjIxNDc0ODM2NDYiLCJuYmYiOjE2ODk2NTk3NDMsImV4cCI6MTY5MDI2NDU0MywiaWF0IjoxNjg5NjU5NzQzfQ.Pa7qAUSF5U2a1SVn5M60CP-RtxkyvofER3cVBbjVSJM"
 function postDataToCoffeeWeb(robustaArray, arabicaArray) {
     let data = robustaArray.concat(arabicaArray);
-    console.log("data", data)
+    // console.log("data", data)
     fetch('https://coffeeweb.org/api/TincapheAuth/InsertTincapheData', {
         method: 'POST',
         headers: {
